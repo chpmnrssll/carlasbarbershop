87f44d3423eb00e3dfcce2e81887f482e8e930c7
Russell Chapman <chpmnrssll@gmail.com>
2019-06-27 23:26:33 -0600

    Updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,78 @@
 <template>
-  <footer class="page-footer font-small teal py-4">
-    <div class="footer-copyright text-center">
-      <g-link class="nav--link" to="/admin">Netlify CMS</g-link>
-    </div>
-  </footer>
+  <FullScreenHero>
+    <template v-slot:background>
+      <g-image src="~/assets/images/bg-img-06.jpg" alt="" />
+    </template>
+    <template v-slot:body>
+      <div class="container-fluid">
+        <b-row>
+          <b-col>
+            <h2 class="mb-4">Opening Hours</h2>
+            <p>Monday – Friday……………………………….9am – 5pm</p>
+            <p>Saturday………………………………………….11am – 3pm</p>
+            <p>Sunday……………………………………………………..Closed</p>
+          </b-col>
+          <b-col>
+            <h2 class="mb-4">Contact</h2>
+            <p>Address…………………1 Fifth Avenue, New York, NY</p>
+            <p>Phone……………………………………….+1 910-626-85255</p>
+            <p>Email………………………………………..info@barber.com</p>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <h2 class="mb-4">Keep In Touch</h2>
+            <div class="">
+              <g-link>FaceBook</g-link>
+            </div>
+            <div class="">
+              <g-link>Twitter</g-link>
+            </div>
+            <div class="">
+              <g-link>Google+</g-link>
+            </div>
+          </b-col>
+        </b-row>
+
+        <div class="copyright d-flex justify-content-between">
+          <p>Copyright © 2019 Carlas Barber Shop | All rights reserved</p>
+          <g-link class="nav--link" to="/admin">Netlify CMS</g-link>
+        </div>
+      </div>
+    </template>
+  </FullScreenHero>
 </template>
 
-<style lang="scss" scoped>
-footer {
-  width: 100%;
-  margin-top: 6rem;
-  background-color: #f5f5f5;
+<script>
+import FullScreenHero from './FullScreenHero.vue';
 
-  .nav--link {
-    color: #20c997;
-  }
+export default {
+  components: {
+    FullScreenHero,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.copyright {
+  height: 100%;
+}
+section {
+  background-color: #000000;
+}
+img {
+  opacity: 0.5;
+}
+h2 {
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: 1.866rem;
+  font-weight: 700;
+  line-height: 1;
+  text-transform: uppercase;
+}
+p {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 100%;
+  line-height: 1.857;
 }
 </style>
