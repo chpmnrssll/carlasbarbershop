d1f42a03689eead8e506b90767ee27ad43e0ef06
Russell Chapman <chpmnrssll@gmail.com>
2019-06-29 14:49:06 -0600

    Added vue-svg-loader

--- gridsome.config.js
+++ gridsome.config.js
@@ -71,6 +71,9 @@ module.exports = {
 
         return options;
       });
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader');
   },
   css: {
     loaderOptions: {
--- src/components/BannerSection.vue
+++ src/components/BannerSection.vue
@@ -4,26 +4,28 @@
       <g-image src="~/assets/images/bg-img-03.jpg" alt="" immediate />
     </template>
     <template v-slot:body>
-      <b-container fluid>
-        <b-row>
+      <b-container>
+        <b-row align-v="center">
           <b-col cols="6">
-            <h3 class="text-light">World-class men's haircuts</h3>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="6">
+            <b-row>
+              <b-col cols="2" class="pr-0">
+                <hr class="my-2" />
+              </b-col>
+              <b-col>
+                <h3 class="text-light">World-class men's haircuts</h3>
+              </b-col>
+            </b-row>
+
             <h1 class="text-light">For gentlemen on the go</h1>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="6">
-            <blockquote class="text-light">
-              If you keep walking past the barbers, eventually you'll get a haircut.
-            </blockquote>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="6">
+
+            <hr class="my-2" />
+            <b-media>
+              <QuoteRight class="quote_right" slot="aside" />
+              <blockquote class="text-light">
+                If you keep walking past the barbers, eventually you'll get a haircut.
+              </blockquote>
+            </b-media>
+
             <b-button @click="" class="mt-4 py-3 px-5 custom-button">
               Book an appointment
             </b-button>
@@ -36,16 +38,29 @@
 
 <script>
 import FullScreenHero from './FullScreenHero.vue';
+import QuoteRight from '~/assets/images/quote_right.svg';
 
 export default {
   components: {
     FullScreenHero,
+    QuoteRight,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.row {
+.container {
   max-width: 1240px;
+  padding-top: 15.625rem;
+}
+.quote_right {
+  color: var(--light);
+  fill: currentColor;
+  width: 64px;
+  height: 64px;
+}
+hr {
+  border-top: 4px solid var(--light);
+  opacity: 0.75;
 }
 </style>
