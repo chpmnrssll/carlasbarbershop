1db60d00fb89d1326f66cda15f15ff248723db1b
Russell Chapman <chpmnrssll@gmail.com>
2019-07-02 01:25:13 -0600

    Wait 1 frame to let the DOM have time to render before calling getComputedStyle

--- src/components/FullScreenHero.vue
+++ src/components/FullScreenHero.vue
@@ -1,10 +1,6 @@
 <template>
   <section>
-    <b-container
-      class="d-flex flex-column justify-content-center mb-0 p-0"
-      :class="overlay ? 'gradient-overlay' : ''"
-      fluid
-    >
+    <b-container class="d-flex flex-column justify-content-center mb-0 p-0" fluid>
       <div :class="overlay ? 'img-under' : ''">
         <slot name="background" />
       </div>
@@ -25,7 +21,6 @@ export default {
   props: {
     className: { type: String, default: '' },
     fixed: { type: Boolean, default: false },
-    overlay: { type: Boolean, default: false },
     sectionHeight: { type: String, default: '' },
   },
   computed: {
@@ -35,36 +30,38 @@ export default {
     },
   },
   mounted() {
-    const sectionHeight = parseFloat(this.sectionHeight, 10);
+    window.requestAnimationFrame(() => {
+      const sectionHeight = parseFloat(this.sectionHeight, 10);
 
-    // set this sections' height to match .FullScreenHero-body
-    const body = this.$el.querySelector('.FullScreenHero-body div');
-    const height = parseInt(window.getComputedStyle(body).height, 10);
-    if (sectionHeight && sectionHeight > height) {
-      this.$el.style.height = this.sectionHeight;
-      body.parentElement.parentElement.style.height = this.sectionHeight;
-    } else {
-      this.$el.style.height = `${height}px`;
-      body.parentElement.parentElement.style.height = `${height}px`;
-    }
+      // set this sections' height to match .FullScreenHero-body
+      const body = this.$el.querySelector('.FullScreenHero-body div');
+      const bodyHeight = parseInt(window.getComputedStyle(body).height, 10);
+      if (sectionHeight && sectionHeight > bodyHeight) {
+        this.$el.style.height = this.sectionHeight;
+        body.parentElement.parentElement.style.height = this.sectionHeight;
+      } else {
+        this.$el.style.height = `${bodyHeight}px`;
+        body.parentElement.parentElement.style.height = `${bodyHeight}px`;
+      }
 
-    if (this.fixed) {
-      // hide the original g-image
-      const bgImg = this.$el.querySelector('.g-image');
-      bgImg.classList.add('d-none');
+      if (this.fixed) {
+        // hide the original g-image
+        const bgImg = this.$el.querySelector('.g-image');
+        bgImg.classList.add('d-none');
 
-      // build a new blank img with src as a css background-image
-      const fixedImg = document.createElement('img');
-      fixedImg.style.width = '100%';
-      fixedImg.style.height = `${height}px`;
-      fixedImg.style.backgroundAttachment = 'fixed';
-      fixedImg.style.backgroundImage = `url(${bgImg.src})`;
-      fixedImg.style.backgroundPosition = 'center';
-      fixedImg.style.backgroundSize = 'cover';
-      fixedImg.style.transform = 'scale(1.01)';
-      fixedImg.alt = bgImg.alt;
-      bgImg.parentElement.append(fixedImg);
-    }
+        // build a new blank img with src as a css background-image
+        const fixedImg = document.createElement('img');
+        fixedImg.style.width = '100%';
+        fixedImg.style.height = `${bodyHeight}px`;
+        fixedImg.style.backgroundAttachment = 'fixed';
+        fixedImg.style.backgroundImage = `url(${bgImg.src})`;
+        fixedImg.style.backgroundPosition = 'center';
+        fixedImg.style.backgroundSize = 'cover';
+        fixedImg.style.transform = 'scale(1.01)';
+        fixedImg.alt = bgImg.alt;
+        bgImg.parentElement.append(fixedImg);
+      }
+    });
   },
 };
 </script>
@@ -79,7 +76,6 @@ section {
   left: 0;
   height: 100%;
   object-fit: cover;
-  // opacity: 0.5;
   pointer-events: none;
   position: absolute;
   top: 0;
@@ -90,15 +86,10 @@ section {
 }
 .container-fluid {
   background-color: transparent;
-  background-image: linear-gradient(225deg, var(--primaryBlue) 0%, var(--primaryGreen) 65%);
-  border-radius: 0 !important;
   left: 0;
   height: 100%;
   overflow: hidden;
   position: absolute;
   width: 100%;
-}
-.gradient-overlay {
-  background-image: linear-gradient(225deg, var(--primaryBlueA) 0%, var(--primaryGreenA) 65%);
 }
 </style>
